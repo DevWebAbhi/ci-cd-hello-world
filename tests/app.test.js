e3190367b38caef3a531cf3930a32dd1b35bf70a
abhishektiwari-maker <abhishek.tiwari@kalvium.community>
2026-02-24 11:52:03 +0530

    done

---
+++ tests/app.test.js
@@ -0,0 +1,13 @@
+const request = require("supertest");
+const app = require("../server");
+
+describe("GET /", () => {
+  it("should return Hello World message", async () => {
+    const response = await request(app).get("/");
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({
+      message: "Hello World from Backend 🚀"
+    });
+  });
+});
